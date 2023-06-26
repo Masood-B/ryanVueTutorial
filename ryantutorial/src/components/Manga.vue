4fff10b6be452735c4aca5eb00c0c1686600a1c8
Masood-B <masoodbasardien123@gmail.com>
2023-06-26 16:49:31 +0200

    1

--- ryantutorial/src/components/Manga.vue
+++ ryantutorial/src/components/Manga.vue
@@ -1,39 +1,61 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="row gap-2">
+        <div class="card" v-for="data in jsonData" style="width: 18rem;" :key="data.id">
+            <h5 class="card-title">{{data.name}}</h5>
+            <p class="card-text">Genre:{{data.genre}}</p>
+        </div>
+    </div>
     <div id="list">
-        <li v-for="mangas in Manga" :key="mangas.id"></li>
+        <!-- <li v-for="mangas in Manga" :key="mangas.id"></li> -->
     </div>
   </div>
 </template>
 
 <script>
-async function jsData() {
-  const response = await fetch(
-    "https://masood-b.github.io/jsonServer/server.json"
-  );
-  const jsonData = await response.json();
+export default{
+    data() {
+        return {
+            jsonData: []
+        }
+    },
+    methods: {
+        async jsData() {
+          const response = await fetch(
+            "https://masood-b.github.io/jsonServer/server.json"
+          );
+          //const jsonData = await response.json();
+          let manga = await response.json()
+            this.jsonData = manga.Manga;
+        //  let info = jsonData.Manga;
+        
+         
+        //   console.log(info);
+        
+        //   const result = document.querySelector("#list");
+        
+        //   info.forEach(data => {
+        //     result.innerHTML =+`
+        //     <div class="card" style="width: 18rem;">
+        //             <img src="https://google.com" class="card-img-top" alt="...">
+        //             <div class="card-body">
+        //               <h5 class="card-title">${data.name}</h5>
+        //               <p class="card-text">Genre:${data.genre}</p>
+        //               <a href="#" class="btn btn-primary">Go somewhere</a>
+        //             </div>
+        //           </div>
+        //     `;
+        //   });
+        }
 
- let info = jsonData.Manga;
+    },
+    created() {
 
- 
-  console.log(info);
-
-  const result = document.querySelector("#list");
-
-  info.forEach(data => {
-    result.innerHTML =+`
-    <div class="card" style="width: 18rem;">
-            <img src="https://google.com" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${data.name}</h5>
-              <p class="card-text">Genre:${data.genre}</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-    `;
-  });
+    },
+    mounted() {
+        this.jsData()
+    }
 }
-jsData();
 
 </script>
 
